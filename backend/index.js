b8f383974c1b710a6caefd395af2fad909343c7a
Abid <abid.geek@outlook.com>
2024-11-02 18:31:18 -0400

    fixed mongodb integration

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,16 @@ let express = require('express');
 let mongoose = require('mongoose');
 let cors = require('cors');
 let bodyParser = require('body-parser');
+let dotenv = require('dotenv').config()
 const app = express();
+
 app.use(cors({allowedHeaders: '*'}));
 
 // Express Route
 
 // Connecting mongoDB Database
 
-const db = process.env.MONGODB_URI
+const db = process.env.MONGODB_URI.toString()
 
 mongoose
   .connect(db)
@@ -21,7 +23,7 @@ mongoose
   })
 
 
-
+/*
 app.route('/').get((req, res, next) => {
   res.json('hello')
 })
@@ -33,26 +35,4 @@ app.use(bodyParser.urlencoded({
 
 
 //app.use(userRoute);
-
-
-
-
-// PORT
-const port = process.env.PORT || 4000;
-const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
-
-
-
-// 404 Error
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
-}); 
-
+*/
